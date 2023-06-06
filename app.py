064404c714cdb55708f579aa856c0a67d8df24da
Taroon <dragsxis@yahoo.com>
2023-06-07 00:47:37 +0100

    hello2

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, jsonify
+from flask import request
 import os
 import sqlite3 as sql
-
+from dotenv import load_dotenv
 # app - The flask application where all the magical things are configured.
 app = Flask(__name__)
+load_dotenv()
 
 # Constants - Stuff that we need to know that won't ever change!
 DATABASE_FILE = "database.db"
@@ -22,56 +24,165 @@ def home():
 #  if it's a POST request process the submitted data
 #  but if it's a GET request, just show the form
 #------------------------------------------------------------
+
+@app.route('/random')
+def random_buggy():
+        import random
+        power_type_options = ['bio', 'electric', 'fusion', 'hamster', 'none', 'petrol', 'rocket', 'solar', 'steam', 'thermo', 'wind']
+        tyres_options = ['knobbly', 'maglev', 'reactive', 'slick']
+        armour_options = ['aluminium', 'thicksteel', 'thinsteel', 'titanium', 'wood']
+        attack_options = ['biohazard', 'charge', 'flame', 'spike']
+        algo_options = ['buggy','defensive','offensive','random','steady','titfortat']
+        flag_color_options = ['white', 'black', 'grey', 'red', 'orange', 'yellow', 'green', 'blue', 'purple']
+        flag_pattern_options = ['plain', 'spots', 'stripes']
+        special_options = ['antibiotic','banging','fireproof','hamster_boosted','insulated']
+
+        qty_wheels = random.choice(range(2, 11, 2))
+        power_type = random.choice(power_type_options)
+        tyres = random.choice(tyres_options)
+        qty_tyres = random.randint(qty_wheels, qty_wheels + 10)
+        armour = random.choice(armour_options)
+        attack = random.choice(attack_options)
+        algo = random.choice(algo_options)
+        flag_color = random.choice(flag_color_options)
+        flag_color_sec = random.choice(flag_color_options)
+        flag_pattern = random.choice(flag_pattern_options)
+        special = random.choice(special_options)
+
+        buggy_data = {
+            'qty_wheels': qty_wheels,
+            'power_type': power_type,
+            'tyres': tyres,
+            'qty_tyres': qty_tyres,
+            'armour': armour,
+            'attack': attack,
+            'algo': algo,
+            'flag_color': flag_color,
+            'flag_color_sec': flag_color_sec,
+            'flag_pattern': flag_pattern,
+            'special': special
+        }
+
+        return buggy_data
+
 @app.route('/new', methods = ['POST', 'GET'])
 def create_buggy():
-    def rules(qty_wheels, power_type, qty_tyres, flag_color, flag_color_sec, flag_pattern):
+    import random
+
+    def validate_random_buggy(buggy_data):
         count = 0
         error_messages = []
 
-        # Check if qty_wheels and qty_tyres have values
+        qty_wheels = buggy_data['qty_wheels']
+        power_type = buggy_data['power_type']
+        qty_tyres = buggy_data['qty_tyres']
+        attack = buggy_data['attack']
+        algo = buggy_data['algo']
+        flag_color = buggy_data['flag_color']
+        flag_color_sec = buggy_data['flag_color_sec']
+        flag_pattern = buggy_data['flag_pattern']
+
+         # Check for if qty_wheels and qty_tyres have values
         if not qty_wheels:
             error_messages.append("Quantity of wheels is required.")
         if not qty_tyres:
             error_messages.append("Quantity of tyres is required.")
 
-        # Check if qty_wheels is even and an integer
+        # Check for if qty_wheels is even and an integer
         if qty_wheels and not qty_wheels.isdigit():
             error_messages.append("Quantity of wheels must be an integer.")
         elif qty_wheels and int(qty_wheels) % 2 != 0:
             error_messages.append("Quantity entered for wheels must be an even value.")
 
-        # Check if qty_tyres is an integer
+        # Check for if qty_tyres is an integer
         if qty_tyres and not qty_tyres.isdigit():
             error_messages.append("Quantity of tyres must be an integer.")
 
-        # Check if power_type is selected
+        # Check for if power_type is selected
         if power_type == 'none':
             error_messages.append("You must choose a Power Type. 'none' is not allowed.")
 
-        # Check if flag_color and flag_color_sec are different
+        # Check for if flag_color and flag_color_sec are different
         if flag_color == flag_color_sec:
             error_messages.append("Colors selected must be different from each other.")
 
-        # Handle 'plain' flag_pattern selection
+        # Check for 'plain' flag_pattern selected
         if flag_pattern == 'plain':
             flag_color_sec = 'none'
 
-        if not error_messages:
-            count = 6  # All checks passed
+        # Check for if either attack or defensive selected or not
+        if attack == 'none':
+            algo = 'defensive' if algo == 'defensive' else 'steady'
+            
 
-        return count, error_messages, qty_wheels, qty_tyres, flag_color, flag_color_sec
+        if not error_messages:
+            count = 7  # All checks passed
+
+        return count, error_messages, qty_wheels, qty_tyres, attack, algo, flag_color, flag_color_sec, flag_pattern
+
+    
+    def rules(qty_wheels, power_type, qty_tyres, attack, algo, flag_color, flag_color_sec, flag_pattern):
+        count = 0
+        error_messages = []
+
+        # Check for if qty_wheels and qty_tyres have values
+        if not qty_wheels:
+            error_messages.append("Quantity of wheels is required.")
+        if not qty_tyres:
+            error_messages.append("Quantity of tyres is required.")
+
+        # Check for if qty_wheels is even and an integer
+        if qty_wheels and not qty_wheels.isdigit():
+            error_messages.append("Quantity of wheels must be an integer.")
+        elif qty_wheels and int(qty_wheels) % 2 != 0:
+            error_messages.append("Quantity entered for wheels must be an even value.")
+
+        # Check for if qty_tyres is an integer
+        if qty_tyres and not qty_tyres.isdigit():
+            error_messages.append("Quantity of tyres must be an integer.")
+
+        # Check for if power_type is selected
+        if power_type == 'none':
+            error_messages.append("You must choose a Power Type. 'none' is not allowed.")
+
+        # Check for if flag_color and flag_color_sec are different
+        if flag_color == flag_color_sec:
+            error_messages.append("Colors selected must be different from each other.")
+
+        # Check for 'plain' flag_pattern selected
+        if flag_pattern == 'plain':
+            flag_color_sec = 'none'
+
+        # Check for if either attack or defensive selected or not
+        if attack == 'none':
+            algo = 'defensive' if algo == 'defensive' else 'steady'
+            
+
+        if not error_messages:
+            count = 7  # All checks passed
+
+        return count, error_messages, qty_wheels, qty_tyres, attack, algo, flag_color, flag_color_sec, flag_pattern
 
 
     def calculate(qty_wheels, power_type, tyres, armour, attack, algo, special, costs):       
-        # Calculate the total cost of the buggy based on choices selected
+        # The total cost of the buggy based on choices selected
+    
         total_cost = costs.get(power_type, 0)
         total_cost += costs.get(tyres, 0)
-        total_cost += costs.get(armour, 0)
         total_cost += costs.get(attack, 0)
         total_cost += costs.get(algo, 0)
         total_cost += costs.get(special, 0)
         # Multiply the total cost by choices where an integer is involved
         total_cost *= int(qty_wheels)
+        # Wheel and Armour cost Percentage equations 
+        if int(qty_wheels) > 4:
+            wheel_cost_percent = (int(qty_wheels) - 4) * 10
+            armour_cost = costs.get(armour, 0)
+            wheel_armour_cost = armour_cost * (wheel_cost_percent / 100)
+            total_cost += wheel_armour_cost
+        elif int(qty_wheels) == 4:
+            armour_cost = costs.get(armour, 0)
+            total_cost += armour_cost
 
         return total_cost
 
@@ -79,6 +190,20 @@ def create_buggy():
         return render_template("buggy-form.html")
     elif request.method == 'POST':
         msg = ""
+        action = request.form.get('action')
+        
+        if action == 'Generate Random Buggy':
+            buggy_data = random_buggy()
+            count, error_messages = validate_random_buggy(buggy_data) 
+            total_cost = calculate(buggy_data)
+
+            
+            if error_messages:
+                msg = " ".join(error_messages)
+                return render_template("buggy-form.html", error_messages=error_messages, msg=msg)
+            
+            return render_template("buggy-form.html", buggy_data=buggy_data, total_cost=total_cost)
+        
         qty_wheels = request.form['qty_wheels']
         power_type = request.form['power_type']
         tyres = request.form['tyres']
@@ -129,9 +254,8 @@ def create_buggy():
         cursor.execute("SELECT valid_check FROM buggies WHERE id = ?", (DEFAULT_BUGGY_ID,))
         row = cursor.fetchone()
         boolean_check = row[0] if row is not None else False
-        count, error_messages, qty_wheels, qty_tyres, flag_color, flag_color_sec = rules(
-            qty_wheels, power_type, qty_tyres, flag_color, flag_color_sec, flag_pattern
-        )
+        count, error_messages, qty_wheels, qty_tyres, attack, algo, flag_color, flag_color_sec, flag_pattern, _ = rules(qty_wheels, power_type, qty_tyres, attack, algo, flag_color, flag_color_sec, flag_pattern)
+
         valid_check = bool(boolean_check or count == 6)
 
         if error_messages:
